b0cdd429857482e44f07a0acff1074dc8a87702f
Camila-Isabela <camila_isabela_lins@hotmail.com>
2022-12-06 10:34:07 -0300

    middleware

--- src/index.js
+++ src/index.js
@@ -1,41 +1,58 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
+//Middleware
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+	
+    const { username } = request.headers
+    const user = users.find(user => user.username === username)
+
+    if(!user) {
+      return response.status(400).json({ error: "User not found!" })
+    }
+
+    request.user = user
+
+    return next()
+
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+
+	const { name, username } = request.body
+
+  const userAlreadyExists = 
+
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+	// Complete aqui
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+	// Complete aqui
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+	// Complete aqui
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+	// Complete aqui
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+	// Complete aqui
 });
 
 module.exports = app;
